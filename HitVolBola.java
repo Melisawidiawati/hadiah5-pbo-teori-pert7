f4cca06d43162dd201994d40dc9327297219e798
Melisawidiawati <90242961+Melisawidiawati@users.noreply.github.com>
2021-11-04 11:53:40 +0800

    Add files via upload

---
+++ HitVolBola.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+class HitVolBola {
+	public static void main(String args[]) {
+	Double  hasil;
+	int jari;
+	
+	// Proses Instansiasi Object baru dari Class RumusBola
+	RumusBola rumus = new RumusBola();
+	
+	
+	Scanner input = new Scanner(System.in);
+		
+	System.out.println("Aplikasi polimorphisme dan inheritance untuk mencari volume bola");
+	System.out.println();
+	System.out.print("Silahkan Masukkan nilai jari-jari / r: ");
+	jari = input.nextInt();
+	System.out.println();
+		
+	// Proses Pemanggilan Method radius dari Class volbola(parent) melalui Class RumusBola(Child)	
+	rumus.radius(jari);
+	
+	
+	// Proses Pemanggilan Method tampil dari Class RumusBola(Child)
+	rumus.tampil();
+	
+	
+	// Proses Pemanggilan Method hitung dari Class RumusBola(Child)
+	rumus.hitung(jari);
+	
+		
+	}	
+}
